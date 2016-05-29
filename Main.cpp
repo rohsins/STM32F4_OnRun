c1046aa26b21ffbb6cd45a1cb58744f82369bbc8
Rohit Singh <rohsins@gmail.com>
2016-05-29 21:20:01 +0545

    rectify usart, spi initializations

--- Main.cpp
+++ Main.cpp
@@ -7,9 +7,12 @@
 #include "RTE_Components.h" 
 #include "stm32f4xx_hal_gpio.h"
 #include "Driver_USART.h"
+#include "Driver_SPI.h"
 
 
-extern  ARM_DRIVER_USART Driver_USART1;
+extern ARM_DRIVER_USART Driver_USART1;
+extern ARM_DRIVER_SPI Driver_SPI1;
+osThreadId tid_mySPI_Thread;
 
 void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct;
@@ -58,7 +61,10 @@ void USART_Handler(uint32_t event) {
 	
 }
 
-void Usart1Initialize(void) {
+void USART1Initialize(void) {
+	
+	__USART1_CLK_ENABLE();
+	
 	Driver_USART1.Control(ARM_USART_MODE_ASYNCHRONOUS | ARM_USART_DATA_BITS_8 | ARM_USART_STOP_BITS_1 | ARM_USART_FLOW_CONTROL_NONE | ARM_USART_PARITY_NONE , 115200);
 	Driver_USART1.Control(ARM_USART_CONTROL_TX, 1);
 	Driver_USART1.Control(ARM_USART_CONTROL_RX, 1);
@@ -68,6 +74,85 @@ void Usart1Initialize(void) {
 	
 	Driver_USART1.Send("hey\n",4);
 }
+
+void mySPI_callback(uint32_t event)
+{
+    switch (event)
+    {
+    case ARM_SPI_EVENT_TRANSFER_COMPLETE:
+        /* Success: Wakeup Thread */
+        osSignalSet(tid_mySPI_Thread, 0x01);
+        break;
+    case ARM_SPI_EVENT_DATA_LOST:
+        /*  Occurs in slave mode when data is requested/sent by master
+            but send/receive/transfer operation has not been started
+            and indicates that data is lost. Occurs also in master mode
+            when driver cannot transfer data fast enough. */
+        __breakpoint(0);  /* Error: Call debugger or replace with custom error handling */
+        break;
+    case ARM_SPI_EVENT_MODE_FAULT:
+        /*  Occurs in master mode when Slave Select is deactivated and
+            indicates Master Mode Fault. */
+        __breakpoint(0);  /* Error: Call debugger or replace with custom error handling */
+        break;
+    }
+}
+
+
+void SPI1Initialize(void) {
+	GPIO_InitTypeDef GPIO_InitStruct;
+	
+	__GPIOE_CLK_ENABLE();
+
+  GPIO_InitStruct.Pin   = GPIO_PIN_3;
+  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull  = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+	
+	__SPI1_CLK_ENABLE();
+	
+	Driver_SPI1.Initialize(mySPI_callback);
+	Driver_SPI1.Control(ARM_SPI_MODE_MASTER | ARM_SPI_CPOL0_CPHA1 | ARM_SPI_MSB_LSB | ARM_SPI_SS_MASTER_SW | ARM_SPI_DATA_BITS(8), 10000000);
+//	Driver_SPI1.Control(ARM_SPI_CONTROL_SS, ARM_SPI_SS_INACTIVE);
+	Driver_SPI1.PowerControl(ARM_POWER_FULL);
+}
+
+void SPIThread(void const *argument) {
+	osEvent evt;
+	const uint8_t testdata_out[1] = {0x8F};
+	uint8_t testdata_in[1];
+	
+	while (1)
+    {
+        /* SS line = ACTIVE = LOW */
+//        Driver_SPI1.Control(ARM_SPI_CONTROL_SS, ARM_SPI_SS_ACTIVE);
+			  GPIOE->BSRR |= 1 << 19;
+        /* Transmit some data */
+        Driver_SPI1.Send(testdata_out, sizeof(testdata_out));
+        /* Wait for completion */
+        evt = osSignalWait(0x01, 100);
+        if (evt.status == osEventTimeout) {
+            __breakpoint(0); /* Timeout error: Call debugger */
+        }
+        /* SS line = INACTIVE = HIGH */
+//        Driver_SPI1.Control(ARM_SPI_CONTROL_SS, ARM_SPI_SS_INACTIVE);
+				GPIOE->BSRR |= 1 << 3;
+        /* SS line = ACTIVE = LOW */
+//        Driver_SPI1.Control(ARM_SPI_CONTROL_SS, ARM_SPI_SS_ACTIVE);
+				GPIOE->BSRR |= 1 << 19;
+        /* Receive 8 bytes of reply */
+        Driver_SPI1.Receive(testdata_in, 1);
+        evt = osSignalWait(0x01, 100);
+        if (evt.status == osEventTimeout) {
+            __breakpoint(0); /* Timeout error: Call debugger */
+        }
+        /* SS line = INACTIVE = HIGH */
+//        Driver_SPI1.Control(ARM_SPI_CONTROL_SS, ARM_SPI_SS_INACTIVE);
+				GPIOE->BSRR |= 1 << 3;
+    }
+}
+osThreadDef(SPIThread, osPriorityNormal, 1, 0);
 
 void Blinky(void const *argument) {
 	while (1) {
@@ -86,10 +171,12 @@ int main (void) {
   osKernelInitialize();
   SystemClock_Config();
 	LEDInitialize();
-	Usart1Initialize();
+	USART1Initialize();
+	SPI1Initialize();
 	
   osKernelStart();
 	osThreadCreate(osThread(Blinky), NULL);
+	osThreadCreate(osThread(SPIThread), NULL);
 	while (1) ;
 
 }
--- RTE/Device/STM32F407VGTx/RTE_Device.h
+++ RTE/Device/STM32F407VGTx/RTE_Device.h
@@ -945,7 +945,7 @@
 
 // <e> SPI1 (Serial Peripheral Interface 1) [Driver_SPI1]
 // <i> Configuration settings for Driver_SPI1 in component ::CMSIS Driver:SPI
-#define RTE_SPI1                        0
+#define RTE_SPI1                        1
 
 //   <o> SPI1_MISO Pin <0=>PA6 <1=>PB4
 #define RTE_SPI1_MISO_PORT_ID           0
